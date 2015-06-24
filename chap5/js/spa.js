7ca6b9efe79e1d4de586abb684f55c3d482a933a
kendou <kenthuang72@gmail.com>
2015-06-24 18:47:23 +0800

    Chapter 5.4.2

--- chap5/js/spa.js
+++ chap5/js/spa.js
@@ -12,7 +12,10 @@
 /*global $, spa */
 
 var spa = (function (){
+  'use strict';
+  
 	var initModule = function ($container){
+	  spa.model.initModule();
 		spa.shell.initModule($container);
 	};
 	
