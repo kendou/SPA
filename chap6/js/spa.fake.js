8b0af13ff991381c8d5f88cfb5e71b9a053852f8
kendou <kenthuang72@gmail.com>
2015-06-30 00:07:46 +0800

    Chap 6.2.2

--- chap6/js/spa.fake.js
+++ chap6/js/spa.fake.js
@@ -13,7 +13,7 @@
 
 spa.fake = (function () {
   'use strict';
-  var getPeopleList, fakeIdSerial, makeFakeId, mockSio;
+  var peopleList, fakeIdSerial, makeFakeId, mockSio;
   
   fakeIdSerial = 5;
   
@@ -21,8 +21,7 @@ spa.fake = (function () {
     return 'id_' + String(fakeIdSerial ++);
   };
   
-  getPeopleList = function (){
-    return [
+  peopleList = [
       { name : 'Betty', _id : 'id_01',
         css_map : {top:20, left: 20,
           'background-color': 'rgb(128,128,128)'
@@ -42,35 +41,55 @@ spa.fake = (function () {
         css_map : {top: 140, left: 20,
           'background-color': 'rgb(192,128,128)'
         }
-      },
-    ];  
-  };
+      }
+  ];
   
   mockSio = (function(){
-    var on_sio, emit_sio, callback_map = {};
+    var on_sio, emit_sio, 
+    send_listchange, listchange_idto,
+    callback_map = {};
     
     on_sio = function(msg_type, callback){
       callback_map[msg_type] = callback;
     };
     
     emit_sio = function(msg_type, data) {
+      var person_map;
       //respond to 'adduser' event with 'userupdate'
       //callback after a 3s delay
       if(msg_type === 'adduser' && callback_map.userupdate){
         setTimeout(function(){
+          person_map = {
+            _id : makeFakeId(),
+            name : data.name,
+            css_map : data.css_map
+          };
+          peopleList.push(person_map);
           callback_map.userupdate(
-            [{ _id : makeFakeId(),
-              name : data.name,
-              css_map : data.css_map
-            }]
+            [person_map]
           );
         }, 3000);
       }
     };
     
+    //Try once per second to use list change callback.
+    //Stop trying after first success.
+    send_listchange = function(){
+      listchange_idto = setTimeout(function(){
+        if(callback_map.listchange){
+          callback_map.listchange([peopleList]);
+          listhange_idto = undefined;
+        }
+        else {send_listchange();}
+      }, 1000);
+    };
+    
+    //We have to start the process ...
+    send_listchange();
+    
     return {emit : emit_sio, on : on_sio};
   }());
-  return {getPeopleList : getPeopleList,
-          mockSio : mockSio
+  
+  return {mockSio : mockSio
   };
 } ());
