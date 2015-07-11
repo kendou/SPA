34958640843625eae2b4c037fcec479306481849
kendou <kenthuang72@gmail.com>
2015-07-11 18:18:48 +0800

    Finish chap 6 and chap 7.2

---
+++ chap7/webapp/app.js
@@ -0,0 +1,52 @@
+/**
+ * Created by kenthuang on 15/7/11.
+ * Simple express server
+ */
+
+//-------------------Begin Module scope variables
+'use strict';
+var
+  http = require('http'),
+  express = require('express'),
+  logger = require('morgan'),
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override'),
+  errorHandler = require('errorhandler'),
+
+  app = express(),
+  router = express.Router(),
+  server = http.createServer(app);
+
+//------------------End Module scope variables
+
+//------------------Begin Server configuration
+
+app.use(bodyParser.json());
+app.use(methodOverride());
+app.use(express.static(__dirname + '/public'));
+app.use(router);
+
+if(app.get('env') === 'development') {
+  app.use(logger('dev'));
+  app.use(errorHandler({
+    dumpExceptions : true,
+    showStack : true
+  }));
+}
+
+if('production' === app.get('env')){
+  app.use(errorHandler());
+}
+
+app.get('/', function(request,response){
+  response.redirect('/spa.html');
+});
+//------------------End Server configuration
+
+//------------------Begin start server
+server.listen(3000);
+console.log(
+  'Express server listenening on port %d in %s mode',
+  server.address().port, app.settings.env
+);
+
