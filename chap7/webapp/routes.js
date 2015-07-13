03c29e400f6f53e88ecc391b1e259ac6c7c489f5
kendou <kenthuang72@gmail.com>
2015-07-13 18:39:02 +0800

    Chap 8.3 finished
    Route.js functions not tested.

--- chap7/webapp/routes.js
+++ chap7/webapp/routes.js
@@ -20,11 +20,9 @@ var configRoutes,
   ),
   dbHandle = new mongodb.Db(
     'spa',mongoServer,{w:1}
-  );
+  ),
+  makeMongoId = mongodb.ObjectID;
 
-dbHandle.open(function(){
-  console.log('** Connected to MongoDB **');
-});
 
 //-------------------------End Module scope variables
 
@@ -40,32 +38,98 @@ configRoutes = function(app, server){
   });
 
   app.get('/:obj_type/list', function(request, response){
-    response.send({title:request.params.obj_type + ' list'});
+    dbHandle.collection(
+      request.params.obj_type,
+      function(outer_error, collection){
+        collection.find().toArray(
+          function(inner_error, map_list){
+            response.send(map_list);
+          }
+        );
+      }
+    );
   });
 
   app.post('/:obj_type/create', function(request, response){
-    response.send({title:'user created'});
+    dbHandle.collection(
+      request.params.obj_type,
+      function(outer_error, collection){
+        var options_map = {safe:true},
+          obj_map = request.body;
+        collection.insert(
+          obj_map,
+          options_map,
+          function(inner_error, result_map){
+            response.send(result_map);
+          }
+        );
+      }
+    );
   });
 
   app.get('/:obj_type/read/:id([0-9]+)', function(request, response){
-    response.send({
-      title: request.params.obj_type + ' with id ' + request.params.id + ' found'
-    });
+    var find_map = {_id: makeMongoId(request.params.id)};
+    dbHndle.collection(
+      request.params.obj_type,
+      function(outer_error, collection){
+        collection.findOne(
+          find_map,
+          function(inner_error, result_map){
+            response.send(result_map);
+          }
+        );
+      }
+    );
   });
 
   app.post('/:obj_type/update/:id([0-9]+)', function(request, response){
-    response.send({
-      title : request.params.obj_type + ' with id ' + request.params.id + ' updated'
-    });
+    var find_map = { _id: makeMongoId(request.params.id)},
+    obj_map = request.body;
+    dbHandle.collection(
+      request.params.obj_type,
+      function(outer_error, collection){
+        var sort_order = [],
+          options_map = {
+            'new': true, upsert: false, safe: true
+          };
+        collection.findAndModify(
+          find_map,
+          sort_order,
+          obj_map,
+          options_map,
+          function(inner_error, updated_map){
+            response.send(updated_map);
+          }
+        );
+      }
+    )
   });
 
   app.get('/:obj_type/delete/:id([0-9]+)', function(request, response){
-    response.send({
-      title: request.params.obj_type + ' with id ' + request.params.id + ' deleted'
-    });
+    var find_map = {_id: makeMongoId(request.params.id)};
+
+    dbHandle.collection(
+      request.params.obj_type,
+      function(outer_error, collection){
+        var options_map = {safe:true, single: true};
+
+        collection.remove(
+          find_map,
+          options_map,
+          function(inner_error, delete_count){
+            response.send({delete_ccount: delete_count});
+          }
+        );
+      }
+    );
   });
 
 };
 
 module.exports = {configRoutes:configRoutes};
 //-------------------------------End public methods
+//-------------------------------Begin Module initialization
+dbHandle.open(function(){
+  console.log('** Connected to MongoDB **');
+});
+//-------------------------------End Module initialization
